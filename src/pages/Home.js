dee3ae1d7dcc111e18bc17ff86e3f931663eb2e1
luisnvf7 <luisnazario2310@gmail.com>
2021-01-24 09:31:25 -0400

    Cambios implementados de search bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useFirestore from '../hooks/useFirestore';
 import Title from '../comps/Title'
 import Navbar from '../comps/Navbar'
@@ -6,39 +6,38 @@ const space = "                            "
 
 const Home = () => {
   const { docs } = useFirestore('books');
+
+  const [docsCopies, setDocsCopies] = useState(null)
+
+  useEffect(() => {
+
+    if (!!docs) {
+      setDocsCopies(docs)
+    }
+
+
+  }, [docs])
+
   for (let x of docs) {
     console.log(x)
   }
 
-  const [term, setTerm] = useState('')
 
-  const handleSearch = (event) => {
-    setTerm(event.target.value)
-    console.log(event.target.value)
-
+  const handleSearch = event => {
+    setDocsCopies(docs.filter(valor => valor.magazineName.toLowerCase().includes(event.target.value.toLowerCase()) ))
   }
-
-  const search = (doc) => {
-    if (term === "") {
-      return doc
-    }
-    else if (doc.magazineName.toLowerCase().includes(term.toLowerCase())) {
-      return doc
-    }
-  }
-
 
   return (
     <div>
       <div>
         <Navbar />
         <Title />
-        <div>
+        <div style = {{ display: 'flex', justifyContent:'center', marginBottom: '2%' }}>
           <input className="searchBar" type="text" placeholder="Busqueda" onChange={handleSearch} />
         </div>
       </div>
       <div className='ContentImg row'>
-        {docs && docs.map(doc => (
+        {docsCopies && docsCopies.map(doc => (
           <div className="col-md-3" style={{ padding: '10px', textAlign: 'center' }} key={doc.id}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/Pdf-2127829.png" alt="uploaded pdf"
               initial={{ opacity: 0 }}
